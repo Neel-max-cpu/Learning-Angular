aaa72f73b10e6a74cfbe52fb07fbc05a4280e8db
Neel Bhattacharya <neel.bhattacharya@hitechglobal>
2025-04-08 16:25:58 +0530

    Added first-ng-app as a regular folder

---
+++ first-ng-app/src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './component/header/header.component';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, HomeComponent, HeaderComponent],
+  template: `
+  <app-header/>
+  <app-home/>
+  `,
+  styles: [`
+    p{
+      background-color: red;
+    }
+    `],
+})
+export class AppComponent {
+  title = 'first-ng-app';
+}
